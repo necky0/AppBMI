f8fb84bc4388ec9ffafb4ecbe30949f8dc020d8d
Unknown <baranekbbm@gmail.com>
2018-03-28 03:29:19 +0200

    Final corrections

--- app/src/main/java/com/example/necky0/bmiapp/MainActivity.java
+++ app/src/main/java/com/example/necky0/bmiapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.necky0.bmiapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,10 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Map;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
